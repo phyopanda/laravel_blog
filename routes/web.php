34837cacc56c68f05417599da1145b2d321cb6ee
Phyo Min Hein <phyominhein2004@gmail.com>
2022-11-04 03:31:19 +0630

    first

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('/articles');
+});
+Route::get('/article', function () {
+    return 'article list';
+})->name('art');
+Route::get('/abc', function () {
+    return redirect('/article');
+});
+Route::get('/ab', function () {
+    return redirect()->route('art');
+});
+Route::get('/employes/{id}', function ($id) {
+    return "employes name - $id";
+});
+Route::get('/articles',[ArticleController::class,'index']);
+
+Route::get('/articles/detail/{id}',[ArticleController::class,'detail']);
+
+Route::get('/articles/add', [ArticleController::class, 'add']);
+
+Route::post('/articles/add', [ArticleController::class,'create']);
+
+Route::get('/articles/delete/{id}', [ArticleController::class,'delete']);
+
+Route::post('/comments/add', [CommentController::class,'create']);
+
+Route::get('/comments/delete/{id}', [CommentController::class,'delete']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
